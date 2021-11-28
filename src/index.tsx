e3e25b3a7d92c35395025b38353f386c50075a73
nsk0826 <nsk.20160401@gmail.com>
2021-11-29 00:03:13 +0900

    メッセージ変更

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 const container = document.getElementById('contents');
 
-ReactDOM.render(<p>こんにちは、世界</p>, container);
+ReactDOM.render(<p>HELLO, JAPAN....</p>, container);
