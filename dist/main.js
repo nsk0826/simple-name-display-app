27af148cefef2bd3955333c48f4a599bcaa2215e
nsk0826 <nsk.20160401@gmail.com>
2021-11-28 23:55:46 +0900

    閉じカッコ位置の修正

--- dist/main.js
+++ dist/main.js
@@ -13,16 +13,16 @@ const createWindow = () => {
     });
     win.loadFile('./index.html');
     win.webContents.openDevTools();
-    electron_1.app.whenReady().then(createWindow);
-    electron_1.app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            electron_1.app.quit();
-        }
-    });
-    electron_1.app.on('activate', () => {
-        if (electron_1.BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
 };
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', () => {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
 //# sourceMappingURL=main.js.map
